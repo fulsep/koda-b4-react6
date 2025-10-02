7eb7c0dbb8e1e276638efd4aa3f931720b296ce9
fulsep <57238803+fulsep@users.noreply.github.com>
2025-10-02 13:32:21 +0700

    fix: formatting and some adjustment to responsiveness

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -8,23 +8,23 @@ import moment from 'moment'
 import { readingTime } from 'reading-time-estimator'
 
 function ArticlePage() {
-  const {username, slug} = useParams()
+  const { username, slug } = useParams()
   const navigate = useNavigate()
   const [data, setData] = React.useState({})
-  const getData = async()=>{
-    const article = await articleFetch(username.slice(1),slug)
-    if(article.length){
+  const getData = async () => {
+    const article = await articleFetch(username.slice(1), slug)
+    if (article.length) {
       setData(article[0])
-    }else{
+    } else {
       navigate("/404")
     }
   }
-  React.useEffect(()=>{
+  React.useEffect(() => {
     getData()
-  },[])
+  }, [])
   const {
-    title="", image=null,
-    author="", date="", body=""
+    title = "", image = null,
+    author = "", date = "", body = ""
   } = data
   return (
     <>
@@ -32,20 +32,20 @@ function ArticlePage() {
         <title>{title} | Medium</title>
       </Helmet>
       <Navbar />
-      <main className='max-w-5xl w-full md:mx-auto mx-5'>
-        <article className='my-20 flex flex-col gap-10'>
+      <main className='max-w-5xl w-full flex justify-center'>
+        <article className='my-20 px-5 flex flex-col gap-10'>
           <h2 className='font-bold md:text-5xl text-3xl'>{title}</h2>
           <div className='flex flex-col md:flex-row md:items-center gap-5'>
             <div className='flex gap-5'>
               <div className='flex gap-5 items-center'>
-              <div>
-                <div className='w-12 aspect-square rounded-full bg-black' />
+                <div>
+                  <div className='w-12 aspect-square rounded-full bg-black' />
+                </div>
+                <div>{author}</div>
               </div>
-              <div>{author}</div>
-            </div>
-            <div>
-              <button type='button' className='bg-white border border-black px-5 py-2 rounded-full'>Follow</button>
-            </div>
+              <div>
+                <button type='button' className='bg-white border border-black px-5 py-2 rounded-full'>Follow</button>
+              </div>
             </div>
             <div className='flex md:flex-row flex-col md:gap-5'>
               <div className='text-gray-600'>{readingTime(body, 250).text}</div>
